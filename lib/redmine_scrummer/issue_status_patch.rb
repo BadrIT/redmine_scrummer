70575c1fc9d6699c38cf16229d08b5c68f38d00b
Mostafa Ali <mostafa.ali@badrit.com>
2011-07-10 17:11:06 +0300

    minor-fix

--- lib/redmine_scrummer/issue_status_patch.rb
+++ lib/redmine_scrummer/issue_status_patch.rb
@@ -5,8 +5,6 @@ module RedmineScrummer
 			base.class_eval do
 				unloadable # Send unloadable so it will not be unloaded in development
 				
-				include InstanceMethods
-	
 			  serialize :scrummer_caption   
       end
       

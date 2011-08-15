863322fccb2857c400b82146b26686b9895807ae
Mahmoud <mahmoud.khaled@badrit.com>
2011-08-15 18:01:48 +0200

    fix migration

--- db/migrate/20110815105331_add_story_size_to_issue.rb
+++ db/migrate/20110815105331_add_story_size_to_issue.rb
@@ -1,7 +1,7 @@
 class AddStorySizeToIssue < ActiveRecord::Migration
   def self.up
     add_column :issues, :story_size, :integer, :default => 0
-    Issue.all.each{|i| i.update_attribute(:story_size, 0)}
+    Issue.all.each{|i| i.update_attribute(:story_size, 0) if i.story_size.nil?}
     
     custom_field = CustomField.find_by_scrummer_caption(:story_size)
     Issue.all.each{|i| i.update_story_size(custom_field)}

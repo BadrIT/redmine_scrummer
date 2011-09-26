565907cc37875cce60614874d1523dfdd3a39dca
Mohamed Elsaka <mohamed.elsaka@badrit.com>
2011-09-26 16:50:52 +0200

    backlog filters icons

--- app/helpers/scrum_sprints_planning_helper.rb
+++ app/helpers/scrum_sprints_planning_helper.rb
@@ -2,8 +2,8 @@ module ScrumSprintsPlanningHelper
   
   include ScrumUserstoriesHelper
   
-  def backlog_filter(tracker_id,label)
-      link_to_remote label,
+  def backlog_filter(tracker_id, icon, label)
+      link_to_remote image_tag( scrummer_image_path(icon), :class => 'filter-icon', :alt => label),
                     :url => {:controller => 'scrum_userstories', :action => 'issues_list', :list_id => 'backlog', :project_id =>@project, :tracker_id => tracker_id},
                     :update => { :success => 'backlog'},
                     :method => 'GET'

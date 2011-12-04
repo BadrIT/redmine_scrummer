ff8334d3e467d7f0f4719b2f28fbc0321a0d0291
Mahmoud <mahmoud.khaled@badrit.com>
2011-12-04 11:47:26 +0200

    move some update code from migration to loader to fix migration

--- db/migrate/20110817124730_update_story_size_to_float.rb
+++ db/migrate/20110817124730_update_story_size_to_float.rb
@@ -3,10 +3,6 @@ class UpdateStorySizeToFloat < ActiveRecord::Migration
     change_table :issues do |t|
       t.change :story_size, :float, :default => 0.0
     end
-    Issue.all.each{|i| i.update_attribute(:story_size, 0.0) if i.story_size.nil?}
-    
-    custom_field = CustomField.find_by_scrummer_caption(:story_size)
-    Issue.all.each{|i| i.update_story_size(custom_field)}
   end
 
   def self.down
--- lib/redmine_scrummer/default_data/loader.rb
+++ lib/redmine_scrummer/default_data/loader.rb
@@ -358,6 +358,11 @@ module RedmineScrummer
                               :name          => l(:start_date),
                               :field_format  => 'date')
                               
+          Issue.all.each{|i| i.update_attribute(:story_size, 0.0) if i.story_size.nil?}
+    
+          custom_field = CustomField.find_by_scrummer_caption(:story_size)
+          Issue.all.each{|i| i.update_story_size(custom_field)}
+                              
           true
         end
       end

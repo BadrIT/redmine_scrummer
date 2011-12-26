915219e32a313fadc50a32041ec04ea7788f2f3d
Mohamed Magdy <mohamed.magdy@badrit.com>
2011-12-26 13:42:00 +0200

    Fixes #1126 adding the story size at the lower right size of the issue in release planning

--- app/helpers/scrum_releases_planning_helper.rb
+++ app/helpers/scrum_releases_planning_helper.rb
@@ -1,12 +1,14 @@
 module ScrumReleasesPlanningHelper
   
   def issue_li_tag(issue)
-      "<li class='issue' id='#{issue.id}'> 
-        <a target='_blank' class='issue #{issue.tracker.short_name.downcase}-issue' href='issues/#{issue.id}'> 
-          <h2>##{issue.id}: #{issue.tracker.short_name}</h2>
-          <p>#{issue.subject}</p> 
-        </a> 
-      </li>"
+    unit = issue.has_custom_field?(:story_size) ? "pt":"hr"
+    "<li class='issue' id='#{issue.id}'> 
+      <a target='_blank' class='issue #{issue.tracker.short_name.downcase}-issue' href='issues/#{issue.id}'> 
+      <h2>##{issue.id}: #{issue.tracker.short_name}</h2>
+      <p>#{truncate(issue.subject, 40, "...")}</p> 
+      <p><span style='color: #444; float: right;'>#{issue.story_size} #{unit}</span></p> 
+      </a> 
+     </li>"
   end
   
 end
